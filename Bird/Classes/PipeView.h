00a38f12623f500c5795d8b5b0950609d3e41fa1
apm24gamer <amwan04@gmail.com>
2014-02-07 03:41:54 -0800

    Added pipes obstacle

---
+++ Bird/Classes/PipeView.h
@@ -0,0 +1,13 @@
+//
+//  PipView.h
+//  Bird
+//
+//  Created by MacCoder on 2/7/14.
+//  Copyright (c) 2014 MacCoder. All rights reserved.
+//
+
+#import "WorldObjectProperties.h"
+
+@interface PipView : WorldObjectProperties
+
+@end
---
+++ Bird/Classes/PipeView.m
@@ -0,0 +1,13 @@
+//
+//  PipView.m
+//  Bird
+//
+//  Created by MacCoder on 2/7/14.
+//  Copyright (c) 2014 MacCoder. All rights reserved.
+//
+
+#import "PipView.h"
+
+@implementation PipView
+
+@end
